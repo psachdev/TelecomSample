ff050d2dc039a2410d26134b3b2d496e8a286b9d
prateeksachdeva <sachdeva852000@gmail.com>
2025-01-29 13:53:09 -0800

    Working telecom sample

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-parcelize")
 }
 
 android {
@@ -50,11 +51,13 @@ android {
 }
 
 dependencies {
+    implementation(libs.accompanist.permissions)
     implementation(libs.androidx.core.telecom)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
